bb5e13719c84f4cacc73cf44d38490c2716ba235
Ben Allfree <ben@benallfree.com>
2015-12-18 14:27:44 -0800

    Server printout update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-var host = "192.168.1.118", port = 33333;
+var crypto = require('crypto');
+
+var host = "127.0.0.1", port = 33333;
 
 var dgram = require( "dgram" );
 
 var server = dgram.createSocket( "udp4" );
 
 server.on( "message", function( msg, rinfo ) {
-    console.log( rinfo.address + ':' + rinfo.port + ' - ' + msg.byteLength + ' bytes received. Echoing back.' );
-    server.send( msg, 0, msg.length, rinfo.port, rinfo.address ); 
+  console.log(crypto.createHash('sha512').update(msg).digest('hex'));
+  server.send( msg, 0, msg.length, rinfo.port, rinfo.address ); 
 });
 server.bind( port, host );
